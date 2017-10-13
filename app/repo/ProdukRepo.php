2ca4f0080eb544d91c65a1e18f2864e739cabe62
agus24 <agusx244@gmail.com>
2017-10-13 18:04:23 +0700

    produk Api

--- app/repo/ProdukRepo.php
+++ app/repo/ProdukRepo.php
@@ -15,8 +15,17 @@ class ProdukRepo
 
     public static function getApi(Produk $produk)
     {
-      return $produk->join('merks','produks.merk_id','merks.id')
+      $data = $produk->join('merks','produks.merk_id','merks.id')
                     ->select('produks.*','merks.nama as nama_merk')
                     ->get();
+      $data = $data->map(function($value, $key) {
+          $gambar = json_decode($value->gambar, true);
+          foreach ($gambar as $key => $value) {
+            $gambar[$key] = asset("storage/images/".$value);
+          }
+          $value->gambar = json_encode($gambar);
+          return $value;
+      });
+      return $data;
     }
 }
