0491c60dc8794a7f9525069a78ca518a21c2ba2a
Gustiawan <agusx244@gmail.com>
2018-01-18 17:19:29 +0700

    compare using table

--- resources/views/front/userCompare.blade.php
+++ resources/views/front/userCompare.blade.php
@@ -4,26 +4,44 @@
 <div class="container">
     <div class="row">
         @if($data->count() > 0)
-            <div class="col-md-4">
+            <table width="100%">
+                <tr>
+                    <td><img src="{{ $data->produk1_gambar }}" width="400px" height="400px"></td>
+                    <td><img src="{{ $data->produk2_gambar }}" width="400px" height="400px"></td>
+                </tr>
+                <tr>
+                    <td><p><b>Nama Produk :</b> {{ $data->produk1_nama }}</p></td>
+                    <td><p><b>Nama Produk :</b> {{ $data->produk2_nama }}</p></td>
+                </tr>
+                <tr>
+                    <td><p><b>Harga Produk :</b> {{ number_format($data->produk1_harga) }}</p></td>
+                    <td><p><b>Harga Produk :</b> {{ number_format($data->produk2_harga) }}</p></td>
+                </tr>
+                <tr>
+                    <td><p><b>Spesifikasi :</b> <br>{!! $data->produk1_spesifikasi !!}</p></td>
+                    <td><p><b>Spesifikasi :</b> <br>{!! $data->produk2_spesifikasi !!}</p></td>
+                </tr>
+            </table>
+            {{-- <div class="col-md-6">
                 <div class="col-md-12">
                     <img src="{{ $data->produk1_gambar }}" width="400px" height="400px">
                 </div>
-                <div class="col-md-12">
+                <div class="col-md-8">
                     <p><b>Nama Produk :</b> {{ $data->produk1_nama }}</p>
                     <p><b>Harga Produk :</b> {{ number_format($data->produk1_harga) }}</p>
                     <p><b>Spesifikasi :</b> <br>{!! $data->produk1_spesifikasi !!}</p>
                 </div>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-6">
                 <div class="col-md-12">
                     <img src="{{ $data->produk2_gambar }}" width="400px" height="400px">
                 </div>
-                <div class="col-md-12">
+                <div class="col-md-8">
                     <p><b>Nama Produk :</b> {{ $data->produk2_nama }}</p>
                     <p><b>Harga Produk :</b> {{ number_format($data->produk2_harga) }}</p>
                     <p><b>Spesifikasi :</b> <br>{!! $data->produk2_spesifikasi !!}</p>
                 </div>
-            </div>
+            </div> --}}
             <div class="col-md-12">
                 <a href="{{ url('/user/compare/clean') }}" class="btn btn-danger btn-block">Clear All</a>
             </div>
